81830ec9333a42fee13e48adab889fd7c018e19d
Sardorbek Omonkulov <somonkulov2018@gmail.com>
2021-08-15 02:07:54 -0400

    Everything working: before adding notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { UserTextBox } from "./components/UserTextBox";
-import { useState } from "react";
 import { Typer2 } from "./components/Typer2";
 
 function App() {
-  return (
-    <div className="App">
-      <Typer2 text="In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without" />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Typer2 text="In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without" />
+		</div>
+	);
 }
 
 export default App;
--- src/components/Typer2.tsx
+++ src/components/Typer2.tsx
@@ -1,121 +1,142 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 
 const InputField = styled.p`
-  background-color: #ddd;
-  width: 50rem;
-  height: 10rem;
-  margin: 2rem auto;
-  padding: 1rem;
-  border-radius: 10px;
-  border: 2px solid #555;
-  transition: all 0.3s;
-  font-size: 1.5rem;
-  font-family: Consolas;
+	background-color: #ddd;
+	width: 50rem;
+	height: 10rem;
+	margin: 2rem auto;
+	padding: 1rem;
+	border-radius: 10px;
+	border: 2px solid #555;
+	transition: all 0.3s;
+	font-size: 1.5rem;
+	font-family: Consolas;
 
-  display: flex;
-  flex-wrap: wrap;
-  column-gap: 1ch;
+	display: flex;
+	flex-wrap: wrap;
+	column-gap: 1ch;
 
-  &:focus {
-    outline: none;
-    background-color: #e1e3f7;
-    border: 2px solid #7622e3;
-  }
+	&:focus {
+		outline: none;
+		background-color: #e1e3f7;
+		border: 2px solid #7622e3;
+	}
+`;
+
+const Caret = styled.div`
+	min-width: 2px;
+	min-height: 1.5rem;
+	background-color: rgb(57, 85, 241);
+	position: absolute;
+	transform: tanslate(100%, -50%);
 `;
 
 interface Props {
-  text: string;
+	text: string;
 }
 
 export const Typer2: React.FC<Props> = ({ text }) => {
-  const [input, setInput] = useState(``);
+	//Me
+	const caretRef = useRef<HTMLDivElement>(null);
+	const lettertRef = useRef<HTMLDivElement>(null);
 
-  function handleKeyDown(evt: React.KeyboardEvent<HTMLDivElement>) {
-    if (evt.ctrlKey || evt.altKey || evt.metaKey) return;
-    if (evt.key.length === 1) {
-      setInput((input) => input + evt.key.toLowerCase());
-    } else if (evt.key === `Backspace`) {
-      setInput((input) => input.slice(0, -1));
-    }
-  }
+	const [input, setInput] = useState(``);
 
-  type Char = {
-    char: string;
-    timeTyped: number | null;
-    correct: boolean | null;
-    untyped: boolean;
-    extra: boolean;
-  };
+	useEffect(() => {
+		if (caretRef.current && lettertRef.current) {
+			caretRef.current.style.top = `${lettertRef.current?.offsetTop}px`;
+			caretRef.current.style.left = `${lettertRef.current?.offsetLeft + 10}px`;
+		}
+	}, [lettertRef, caretRef, input]);
 
-  function wordDiff(word1: string, word2: string): Char[] {
-    let result: Char[] = [];
-    for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
-      let char1 = word1[i];
-      let char2 = word2[i];
+	function handleKeyDown(evt: React.KeyboardEvent<HTMLDivElement>) {
+		if (evt.ctrlKey || evt.altKey || evt.metaKey) return;
+		if (evt.key.length === 1) {
+			setInput((input) => input + evt.key);
+		} else if (evt.key === `Backspace`) {
+			setInput((input) => input.slice(0, -1));
+		}
+	}
 
-      if (char1 === undefined) {
-        result.push({
-          char: char2,
-          timeTyped: null,
-          correct: null,
-          untyped: true,
-          extra: false,
-        });
-      } else if (char2 === undefined) {
-        result.push({
-          char: char1,
-          timeTyped: Date.now(),
-          correct: false,
-          untyped: false,
-          extra: true,
-        });
-      } else if (char1 === char2) {
-        result.push({
-          char: char1,
-          timeTyped: Date.now(),
-          correct: true,
-          untyped: false,
-          extra: false,
-        });
-      } else if (char1 !== char2) {
-        result.push({
-          char: char2,
-          timeTyped: Date.now(),
-          correct: false,
-          untyped: false,
-          extra: false,
-        });
-      }
-    }
+	type Char = {
+		char: string;
+		timeTyped: number | null;
+		correct: boolean | null;
+		untyped: boolean;
+		extra: boolean;
+	};
 
-    return result;
-  }
+	function wordDiff(word1: string, word2: string): Char[] {
+		let result: Char[] = [];
+		for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
+			let char1 = word1[i];
+			let char2 = word2[i];
 
-  console.log(input);
-  return (
-    <InputField onKeyDown={handleKeyDown} tabIndex={0}>
-      {text.split(` `).map((word, i) => {
-        const diff = wordDiff(input.split(` `)[i] || ``, word);
+			if (char1 === undefined) {
+				result.push({
+					char: char2,
+					timeTyped: null,
+					correct: null,
+					untyped: true,
+					extra: false,
+				});
+			} else if (char2 === undefined) {
+				result.push({
+					char: char1,
+					timeTyped: Date.now(),
+					correct: false,
+					untyped: false,
+					extra: true,
+				});
+			} else if (char1 === char2) {
+				result.push({
+					char: char1,
+					timeTyped: Date.now(),
+					correct: true,
+					untyped: false,
+					extra: false,
+				});
+			} else if (char1 !== char2) {
+				result.push({
+					char: char2,
+					timeTyped: Date.now(),
+					correct: false,
+					untyped: false,
+					extra: false,
+				});
+			}
+		}
 
-        return (
-          <span key={i}>
-            {diff.map((char, j) => {
-              let color = `black`;
-              if (char.untyped) color = `#888`;
-              if (char.correct === false) color = `red`;
-              if (char.extra) color = `maroon`;
+		return result;
+	}
 
-              return (
-                <span key={`${i}-${j}`} style={{ color }}>
-                  {char.char}
-                </span>
-              );
-            })}
-          </span>
-        );
-      })}
-    </InputField>
-  );
+	return (
+		<div>
+			<Caret ref={caretRef} />
+			<InputField onKeyDown={handleKeyDown} tabIndex={0}>
+				{text.split(` `).map((word, i) => {
+					const diff = wordDiff(input.split(` `)[i] || ``, word);
+
+					return (
+						<span key={i}>
+							{diff.map((char, j) => {
+								let color = `black`;
+								if (char.untyped) color = `#888`;
+								if (char.correct === false) color = `red`;
+								if (char.extra) color = `maroon`;
+
+								return (
+									<span key={`${i}-${j}`} style={{ color }} ref={char.untyped ? null : lettertRef}>
+										{char.char}
+									</span>
+								);
+							})}
+						</span>
+					);
+				})}
+			</InputField>
+		</div>
+	);
 };
